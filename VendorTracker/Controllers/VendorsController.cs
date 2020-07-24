608d12572c6955d1dfe759a6639816473d8b79d8
Taylor Phillips <taylorphillips133@gmail.com>
2020-07-24 11:27:00 -0700

    show a proper passing test for my vendor contstructor

--- VendorTracker/Controllers/VendorsController.cs
+++ VendorTracker/Controllers/VendorsController.cs
@@ -8,50 +8,50 @@ namespace VendorTracker.Controllers
   public class VendorsController : Controller
   {
 
-    [HttpGet("/vendors")]
-    public ActionResult Index()
-    {
-      List<Vendor> allVendors = Vendor.GetAll();
-      return View(allVendors);
-    }
+//     [HttpGet("/vendors")]
+//     public ActionResult Index()
+//     {
+//       List<Vendor> allVendors = Vendor.GetAll();
+//       return View(allVendors);
+//     }
 
-    [HttpGet("/vendors/new")]  
-    public ActionResult New()  //Standard naming convention for a form to create new object
-    {                           // "New" a part of Restful Routing
-      return View();
-    }
+//     [HttpGet("/vendors/new")]  
+//     public ActionResult New()  //Standard naming convention for a form to create new object
+//     {                           // "New" a part of Restful Routing
+//       return View();
+//     }
 
-    [HttpPost("/vendors")]
-    public ActionResult Create(string vendorName)
-    {
-      Vendor newVendor = new Vendor(vendorName);
-      return RedirectToAction("Index");
-    }
+//     [HttpPost("/vendors")]
+//     public ActionResult Create(string vendorName)
+//     {
+//       Vendor newVendor = new Vendor(vendorName);
+//       return RedirectToAction("Index");
+//     }
 
-    [HttpGet("/vendors/{id}")]   // Goes to id that is linked to page, of id passed into Show()
-    public ActionResult Show(int id)
-    {
-      Dictionary<string, object> model = new Dictionary<string, object>();
-      Vendor selectedVendor = Vendor.Find(id);
-      List<Order> vendorOrders = selectedVendor.Orders;
-      model.Add("vendor", selectedVendor);
-      model.Add("orders", vendorOrders);
-      return View(model);  //Passing in a dictionary object to View
-    }
+//     [HttpGet("/vendors/{id}")]   // Goes to id that is linked to page, of id passed into Show()
+//     public ActionResult Show(int id)
+//     {
+//       Dictionary<string, object> model = new Dictionary<string, object>();
+//       Vendor selectedVendor = Vendor.Find(id);
+//       List<Order> vendorOrders = selectedVendor.Orders;
+//       model.Add("vendor", selectedVendor);
+//       model.Add("orders", vendorOrders);
+//       return View(model);  //Passing in a dictionary object to View
+//     }
 
-    // This one creates new Items within a given Category, not new Categories:
-    [HttpPost("/vendors/{vendorId}/orders")]
-    public ActionResult Create(int vendorId, string orderDescription)
-    {
-      Dictionary<string, object> model = new Dictionary<string, object>();
-      Vendor foundVendor = Vendor.Find(vendorId);
-      Order newOrder = new Order(orderDescription);
-      foundVendor.AddOrder(newOrder);
-      List<Order> vendorOrders = foundVendor.Orders;
-      model.Add("orders", vendorOrders);
-      model.Add("vendor", foundVendor);
-      return View("Show", model);
-    }
+//     // This one creates new Items within a given Category, not new Categories:
+//     [HttpPost("/vendors/{vendorId}/orders")]
+//     public ActionResult Create(int vendorId, string orderDescription)
+//     {
+//       Dictionary<string, object> model = new Dictionary<string, object>();
+//       Vendor foundVendor = Vendor.Find(vendorId);
+//       Order newOrder = new Order(orderDescription);
+//       foundVendor.AddOrder(newOrder);
+//       List<Order> vendorOrders = foundVendor.Orders;
+//       model.Add("orders", vendorOrders);
+//       model.Add("vendor", foundVendor);
+//       return View("Show", model);
+//     }
 
   }
 }
