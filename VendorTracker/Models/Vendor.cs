e134dd9ee130b512512f97d993032ef261c4aa57
Taylor Phillips <taylorphillips133@gmail.com>
2020-07-24 10:16:54 -0700

    remove my logic for order and vendor so as to build using TDD

--- VendorTracker/Models/Vendor.cs
+++ VendorTracker/Models/Vendor.cs
@@ -2,35 +2,6 @@ using System.Collections.Generic;
 
 namespace VendorTracker.Models
 {
-  public class Category   // This category will be a vendor 
+  public class Vendor   // This category will be a vendor 
   {
-    private static List<Category> _instances = new List<Category> {};
-    public string Name { get; set; }
-    public int Id { get; }
-    public List<Item> Items { get; set; }
-
-    public Category(string categoryName)
-    {
-      Name = categoryName;
-      _instances.Add(this);
-      Id = _instances.Count;
-      Items = new List<Item> { };
-    }
-    public static void ClearAll()
-    {
-      _instances.Clear();
-    }
-    public static List<Category> GetAll()
-    {
-      return _instances;
-    }
-    public static Category Find(int searchId)
-    {
-      return _instances[searchId - 1];
-    }
-    public void AddItem(Item item)
-    {
-      Items.Add(item);
-    }
-  }
-}
+    
