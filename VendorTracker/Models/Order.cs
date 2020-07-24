43027cf5a0a61ada65ef296feef0fd6513c69ab4
Taylor Phillips <taylorphillips133@gmail.com>
2020-07-24 14:57:10 -0700

    show a passing test for GetId of an Order

--- VendorTracker/Models/Order.cs
+++ VendorTracker/Models/Order.cs
@@ -5,7 +5,7 @@ namespace VendorTracker.Models
   public class Order
   {
     public string Description {get; set;}
-    // public int Id {get;}
+    public int Id {get;}
     private static List<Order> _instances = new List<Order>{};
 
     public Order(string description)
