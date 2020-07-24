76de8561dc8becceaee684f238d0391fa8fc7ec9
Taylor Phillips <taylorphillips133@gmail.com>
2020-07-24 13:41:06 -0700

    show a passing test for adding an order to the vendor

--- VendorTracker/Models/Order.cs
+++ VendorTracker/Models/Order.cs
@@ -4,7 +4,16 @@ namespace VendorTracker.Models
 {
   public class Order
   {
-    
+    public string Description {get; set;}
+    public int Id {get;}
+    private static List<Order> _instances = new List<Order>{};
+
+    public Order(string description)
+    {
+      Description = description;
+      _instances.Add(this);
+      Id = _instances.Count;
+    }
 
   }
 }
